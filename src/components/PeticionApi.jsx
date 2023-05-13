21b6c55d99acfcea5b7574f899ac5a0fdbdab753
Roberto Morales <roberto@morales.com.co>
2023-05-13 16:04:49 -0500

    Componente Variables y Contador

---
+++ src/components/PeticionApi.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeticionApi = () => {
+  return (
+    <div>
+        <h1>PETICIÓN AL API DE RICK AND MORTY</h1>
+    </div>
+  )
+}
+
+export default PeticionApi
--- src/components/Variables.jsx
+++ src/components/Variables.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const Variables = () => {
-  return (
-    <div>Variables</div>
+    const saludo = "Hola Mundo"
+    //const imagen ="https://cdn.resfu.com/img_data/estadios/original_new/547.jpg?size=720x&lossy=1"
+    const imagen = "https://picsum.photos/500"
+    const texto_alt = "Esto es una imagen"
+    
+    return (
+    <div>
+        <h1>{saludo}</h1>
+        <img src={imagen} alt={texto_alt} />
+    </div>
   )
 }
 
