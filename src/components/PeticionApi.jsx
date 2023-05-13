0004a9adc03a059c6b4d819194b452f26543d8e7
Roberto Morales <roberto@morales.com.co>
2023-05-13 16:35:29 -0500

    Petición al api de Rick and Morty

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
 const PeticionApi = () => {
+    const [personajes, setPersonajes] = React.useState([])
+    const [paginacion, setPaginacion] = React.useState(0)
+
+    const traerPersonajes = async() =>{
+        try{
+            const res = await fetch(`https://rickandmortyapi.com/api/character/?page=${paginacion}`)
+            const {results} = await res.json()
+            setPersonajes(results)
+        }catch(error){
+            console.log(error)
+        }   
+    }
+
+    const siguiente = () =>{
+        setPaginacion(paginacion+1)
+        traerPersonajes()
+    }
+
+    const atras = () =>{
+        setPaginacion(paginacion-1)
+        traerPersonajes()
+    }
   return (
     <div>
         <h1>PETICIÓN AL API DE RICK AND MORTY</h1>
+        <button onClick={traerPersonajes}>Traer Personajes</button>
+        <button onClick={atras}>Atrás</button>
+        <button onClick={siguiente}>Siguiente</button>
+        
+        {
+            personajes.map(({id, name, image}) => (
+                <div key= {id}>
+                    <h4>{id} - {name}</h4>
+                    <img src={image} alt={name} />
+                </div>
+            ))
+        }
+        
     </div>
   )
 }
